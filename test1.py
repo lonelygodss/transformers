45b3909f780bd3446ef735a1eb2c94fb24a94ad5
Glados <d202281013@hust.edu.cn>
2024-11-20 17:52:52 +0800

    尝试可视化

--- test1.py
+++ test1.py
@@ -6,6 +6,5 @@ model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
 
 input_text = "translate English to German: How old are you?"
 input_ids = tokenizer(input_text, return_tensors="pt").input_ids
-
 outputs = model.generate(input_ids)
 print(tokenizer.decode(outputs[0]))
